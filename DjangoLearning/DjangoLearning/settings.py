611661fa14cdb0fe0749e387df364ff6f38764dd
ngoctrinh217 <zokute0000@gmail.com>
2024-08-08 22:43:43 +0700

    Update(08/08/2024): update some template.

--- DjangoLearning/DjangoLearning/settings.py
+++ DjangoLearning/DjangoLearning/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,9 @@ ROOT_URLCONF = 'DjangoLearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DjangoLearning/base/views.py
+++ DjangoLearning/base/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+rooms = [
+    {
+        'id': 1, 'name': 'Lets learning with python'
+    },
+    {
+        'id': 2, 'name': 'Design with me'
+    },
+    {
+        'id': 3, 'name': 'Frontend with python'
+    }
+]
+
+
+def home(request):
+    return render (request, 'home.html', {'rooms': rooms})
+def rooms(request):
+    return render (request, 'rooms.html')
